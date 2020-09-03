b8c42d71d9d996379bf8bfdd8e1d14975f09405a
ryo314 <morokosi3142@gmail.com>
2020-09-03 22:49:31 +0900

    エラー出てるよ

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        val button = findViewById<Button>(R.id.button)
+                // ボタンタップ時の処理を指定する
+                button.setOnClickListener {
+            // EditTextへの参照を取得する
+            val editText = findViewById<EditText>(R.id.editText)
+                    // メッセージを取り出す
+                    val message = editText.text.toString()
+
+            // Intentを作成する
+            val intent = Intent(this, ResultActivity::class.java)
+            // パラメータをセットする
+            intent.putExtra("message", message)
+            // 画面を遷移させる
+            startActivity(intent)
+        }
     }
     //テスト倉島
 }
