bd45efa77ceef85381203b0e24699cf841827b9c
tzozmzoz <tuunaiki333@gmail.com>
2020-10-08 23:43:45 +0900

    enum 修正

--- app/src/main/java/com/example/myapplication/Enum/QuestionType.java
+++ app/src/main/java/com/example/myapplication/Enum/QuestionType.java
@@ -3,11 +3,15 @@ package com.example.myapplication.Enum;
 public enum QuestionType {
     BUNBUN("ぶんぶんしますか？", 10, 0),
     EMOTION_LESS("感情を失いましたか？", 0, 10);
+
     private String question;
     private int cookingPoint;
     private int emotionLessPoint;
 
     private QuestionType(String question, int cookingPoint, int emotionLessPoint) {
+        this.question = question;
+        this.cookingPoint = cookingPoint;
+        this.emotionLessPoint = emotionLessPoint;
     }
 
     public String getQuestion(){
