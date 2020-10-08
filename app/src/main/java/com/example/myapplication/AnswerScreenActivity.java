3452a441c189794c79a8c06bc2aa0be2f4cca396
miyagawa <nmm23200@gmail.com>
2020-10-08 22:06:45 +0900

    正解画面　作成中（ボタン配置）
    Signed-off-by: miyagawa <nmm23200@gmail.com>

--- app/src/main/java/com/example/myapplication/AnswerScreenActivity.java
+++ app/src/main/java/com/example/myapplication/AnswerScreenActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.content.Intent;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
         import android.widget.TextView;
 
 public class AnswerScreenActivity extends AppCompatActivity {
@@ -12,12 +15,28 @@ public class AnswerScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_screen);
 
-        String message = getIntent().getStringExtra("message");
+        Button yesButton = findViewById(R.id.yes_button);
+
+        //String message = getIntent().getStringExtra("message");
+        String message = "テスト";
 
         // TextViewへの参照を取得する
         TextView textView = findViewById(R.id.question_text);
 
         // TextViewにテキストを設定する
         textView.setText(message);
+
+        // 「はい」ボタン処理
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intentを作成する
+                Intent intent = new Intent(StartScreenActivity.this, AnswerScreenActivity.class);
+
+                // 画面を遷移させる
+                startActivity(intent);
+
+            }
+        });
     }
 }
