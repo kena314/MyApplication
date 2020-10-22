686cf7e030dfebaf646349c796ca42547977b08f
tzozmzoz <tuunaiki333@gmail.com>
2020-10-22 22:28:52 +0900

    enum設定

--- app/src/main/java/com/example/myapplication/Enum/QuestionType.java
+++ app/src/main/java/com/example/myapplication/Enum/QuestionType.java
@@ -2,41 +2,38 @@ package com.example.myapplication.Enum;
 
 import java.util.Arrays;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public enum QuestionType {
-    BUNBUN(      0,10, 0, "ぶんぶんしますか？"),
-    EMOTION_LESS(1,0, 10, "感情を失いましたか？"),
-    FUTURE_HOPE( 2,0, 10, "将来を期待されていますか?"),
-    BLACK_POOP(  3,0, 10, "ウンコが黒いですか?"),
-    ECSTASY(     4,0, 10, "昇天してる？"),
-    CLANNAD_LIFE(5,0, 10, "クラナドは人生?"),
-    OHTANI(      6,0, 10, "球速165km/sを投げますか?"),
-    WOMAN_GORORI(7,0, 10, "女性ですか？"),
-    MOKOU(       8,0, 10, "勇気の切断"),
-    GORORI(      9,0, 10, "教育番組に出ていますか？");
+    BUNBUN(      0,10, 0,0,0,0,0,0,0,0,0, "ぶんぶんしますか？"),
+    EMOTION_LESS(1,0, 10,0,0,0,0,0,0,0,0, "感情を失いましたか？"),
+    FUTURE_HOPE( 2,0, 0,10,0,0,0,0,0,0,0, "将来を期待されていますか?"),
+    BLACK_POOP(  3,0, 0,0,10,0,0,0,0,0,0, "ウンコが黒いですか?"),
+    ECSTASY(     4,0, 0,0,0,10,0,0,0,0,0, "昇天してる？"),
+    CLANNAD_LIFE(5,0, 0,0,0,0,10,0,0,0,0, "クラナドは人生?"),
+    OHTANI(      6,0, 0,0,0,0,0,10,0,0,0, "球速165km/sを投げますか?"),
+    WOMAN_GORORI(7,0, 0,0,0,0,0,0,10,0,0, "女性ですか？"),
+    MOKOU(       8,0, 0,0, 0,0,0,0,0,10,0,"勇気の切断"),
+    GORORI(      9,0, 0,0,0,0,0,0,0,0,10, "教育番組に出ていますか？");
 
     private int questionNo;
     private int cookingPoint;
     private int emotionLessPoint;
+    private int futurePoint;
+    private int blackPoint;
+    private int ecstasyPoint;
+    private int clannadPoint;
+    private int ohtaniPoint;
+    private int womanGororiPoint;
+    private int mokouPoint;
+    private int gororiPoint;
+
     private String question;
-
-    private QuestionType(int questionNo, int cookingPoint, int emotionLessPoint, String question) {
-        this.questionNo = questionNo;
-        this.cookingPoint = cookingPoint;
-        this.emotionLessPoint = emotionLessPoint;
-        this.question = question;
-    }
-
-    public int getQuestionNo(){ return this.questionNo; }
-
-    public int getCookingPoint(){
-        return this.cookingPoint;
-    }
-
-    public int getEmotionLessPoint(){
-        return this.emotionLessPoint;
-    }
-
-    public String getQuestion(){ return this.question; }
 
     public static QuestionType toQuestionNo(int questionNo){
         return Arrays.stream(QuestionType.values()).filter(it->it.questionNo == questionNo).findFirst().orElse(null);
