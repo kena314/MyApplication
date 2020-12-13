e0a8dadfd3eeabbe4fb6435ae0710aa2e0afa120
tzozmzoz <tuunaiki333@gmail.com>
2020-12-13 16:03:47 +0900

    bgmをつけたかったマン

--- app/src/main/java/com/example/myapplication/StartScreenActivity.java
+++ app/src/main/java/com/example/myapplication/StartScreenActivity.java
@@ -3,12 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import lombok.SneakyThrows;
 
 
 public class StartScreenActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +29,8 @@ public class StartScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button startButton = findViewById(R.id.start_button);
+
+        audioPlay();
 
         // スタートボタン処理
         startButton.setOnClickListener(new View.OnClickListener() {
@@ -29,4 +44,85 @@ public class StartScreenActivity extends AppCompatActivity {
             }
         });
     }
+
+    private boolean audioSetup(){
+        // インタンスを生成
+        mediaPlayer = new MediaPlayer();
+
+        //音楽ファイル名, あるいはパス
+        String filePath = "bgm.mp3";
+
+        boolean fileCheck = false;
+
+        // assetsから mp3 ファイルを読み込み
+        try(AssetFileDescriptor afdescripter = getAssets().openFd(filePath))
+        {
+            // MediaPlayerに読み込んだ音楽ファイルを指定
+            mediaPlayer.setDataSource(afdescripter.getFileDescriptor(),
+                    afdescripter.getStartOffset(),
+                    afdescripter.getLength());
+            // 音量調整を端末のボタンに任せる
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+            //再生準備
+            mediaPlayer.prepare();
+            fileCheck = true;
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        return fileCheck;
+    }
+
+
+    private void audioPlay() {
+
+        if (mediaPlayer == null) {
+            // audio ファイルを読出し
+            if (audioSetup()){
+                Toast.makeText(getApplication(), "Rread audio file", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(getApplication(), "Error: read audio file", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        else{
+            // 繰り返し再生する場合
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            // リソースの解放
+            mediaPlayer.release();
+        }
+
+        // 再生する
+        mediaPlayer.start();
+
+        // 終了を検知するリスナー
+//        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//            @Override
+//            public void onCompletion(MediaPlayer mp) {
+//                Log.d("debug","end of audio");
+//                audioStop();
+//            }
+//        });
+        // lambda
+        mediaPlayer.setOnCompletionListener( mp -> {
+            Log.d("debug","end of audio");
+            audioStop();
+        });
+
+    }
+
+    private void audioStop() {
+        // 再生終了
+        mediaPlayer.stop();
+        // リセット
+        mediaPlayer.reset();
+        // リソースの解放
+        mediaPlayer.release();
+
+        mediaPlayer = null;
+    }
+
+
 }
